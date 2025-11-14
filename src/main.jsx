f85230bde3bf9d2ab2be8902122c2070642e7655
Geremias Acosta <gereacosta1@gmail.com>
2025-11-14 11:15:31 -0500

    try to fix modal2

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/globals.css'
 
-import { initAffirm } from './utils/affirmConfig';
-
-initAffirm();
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
